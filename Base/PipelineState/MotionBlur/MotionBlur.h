2f9840ddc8c61771bc064bcdabfa4a1f96581ff1
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-21 11:23:48 +0900

    add Function MotionBlur

--- Base/PipelineState/MotionBlur/MotionBlur.h
+++ Base/PipelineState/MotionBlur/MotionBlur.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "Base/PipelineState/IPipelineStateObject.h"
+#include "Base/Manager/PSOManager.h"
+
+class MotionBlur : public IPipelineStateObject
+{
+public:
+
+	void ShaderCompile() override;
+	void CreateRootSignature() override;
+	void CreateInputLayOut() override;
+	void CreateBlendState() override;
+	void CreateRasterizarState() override;
+	void CreatePipelineStateObject() override;
+
+private:
+
+};
