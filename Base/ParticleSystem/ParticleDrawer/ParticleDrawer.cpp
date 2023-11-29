28f854aea324c449c7fbc29b94013d079bae4bdd
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-29 18:12:13 +0900

    あ

---
+++ Base/ParticleSystem/ParticleDrawer/ParticleDrawer.cpp
@@ -0,0 +1,18 @@
+#include "ParticleDrawer.h"
+
+void ParticleDrawer::Initalize(int particleVolume, const std::string filePath, Vector3 Pos)
+{
+
+}
+
+void ParticleDrawer::Update()
+{
+
+		particle[0]
+	
+}
+
+void ParticleDrawer::Draw(const ViewProjection& viewProjection)
+{
+
+}
--- Base/ParticleSystem/ParticleDrawer/ParticleDrawer.h
+++ Base/ParticleSystem/ParticleDrawer/ParticleDrawer.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "ParticleSystem/Particle.h"
+#include <array>
+
+class ParticleDrawer
+{
+public:
+	void Initalize(int particleVolume, const std::string filePath, Vector3 Pos);
+
+	void Update();
+
+	void Draw(const ViewProjection& viewProjection);
+
+
+private:
+	std::array<std::unique_ptr<Particle>,20> particle;
+};
